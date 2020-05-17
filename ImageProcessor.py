fed9fa47d105f07ab3616a04f57ffa7731258c6f
choterminater <sunghoon123@hotmail.ca>
2020-05-16 17:10:14 -0700

    Update ImageProcessor.py
    Introduction of JavaGateway
    
    [REMEMBER] to run the Java Program first as the Java program works as a server.
    
    OCR test not included

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -1,4 +1,5 @@
 import os.path
+from py4j.java_gateway import JavaGateway
 
 #This function processes the image and return the appropriate image
 
@@ -6,20 +7,24 @@ import os.path
 #Output: String (name of the original/updated image)
 
 def imageProcessor(image, filterRequested):
+    decidedImage = image
 #(Step 1) Given an image name, if it fails to find the image, error must be raised.
     if not os.path.isfile(image):
         raise IOError
-
-#At this step, you passed the IO test
 
 #(Step 2) Using OCR, verify whether the image is for the advertisement purpose or not.
 
 
 #(Step 3) Using Java, Convert image from original to Grayscale without modifying the original image
+    if filterRequested:
+        gateway = JavaGateway()
+        convertImage = gateway.entry_point
+        convertImage.runImageProcessing(image)
+        decidedImage = 'Filtered.png'
+        gateway.close()
     
-
 #based on the "filterRequest" Boolean, send the name of original/updated image
+    return decidedImage
 
-    return ""
-
-imageProcessor('DEMO.jpg', True)
+#Tester
+print(imageProcessor('DEMO.jpg', True))
