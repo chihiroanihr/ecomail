d4a387e0b482db92c8fe3454a4e78dfb4c451b7b
Rhina Kim <chihiroanihr@gmail.com>
2020-05-18 03:44:12 -0400

    new changes

---
+++ src/src/Grayscale.java
@@ -0,0 +1,47 @@
+import java.awt.*;
+public class Grayscale implements Runnable {
+
+    Thread t;
+    Image img;
+    int[] pixel;
+
+    Grayscale(Image target){
+        img = target;
+        t = new Thread(this);
+        t.start();
+    }
+
+    public void run(){
+        try{
+            while(true) {
+                //get the coordinate with RGB value atomically
+                pixel = img.getRGBWithCoordinate();
+
+                //done!
+                if (pixel == null) break;
+
+                //change the RGB value
+                Color c = new Color(img.getRGBValue(pixel[0],pixel[1]));
+                int red = c.getRed();
+                int green = c.getGreen();
+                int blue = c.getBlue();
+
+                //To Grayscale
+                int average = (red+blue+green)/3;
+                red = blue = green = average;
+
+                c = new Color(average, average, average);
+                pixel[2] = c.getRGB();
+
+                //set the new RGB value
+                img.setPixelonOutputImage(pixel);
+
+                //sleep?
+                Thread.sleep(0);
+            }
+        }
+        catch(InterruptedException e){
+            System.out.println("Failed");
+        }
+    }
+}
